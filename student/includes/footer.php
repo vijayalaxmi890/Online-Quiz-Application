02cae6ce993c0907060b7c55cdbe1792ee16a3a0
Vijayalaxmi <vijaya24042003@gmail.com>
2026-02-21 17:38:58 +0530

    Initial commit

---
+++ student/includes/footer.php
@@ -0,0 +1,4 @@
+﻿ <div style="   position: fixed;left: 0;bottom: 0;width: 100%;background-color: #37474f;color: white;text-align: center;padding-top:15px;">
+  
+<p>© 2022 Quick Quizz | Government Polytechnic MasabTank | Department of Computer Sciences | </p>
+</div>
